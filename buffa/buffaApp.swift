2cb275b6930d55c0fe7d50268ed88abaeb2218ad
zzacal <zac.zacal@alaskaair.com>
2020-11-18 16:52:30 -0800

    Initial Commit

---
+++ buffa/buffaApp.swift
@@ -0,0 +1,17 @@
+//
+//  buffaApp.swift
+//  buffa
+//
+//  Created by Zac Zacal on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct buffaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
