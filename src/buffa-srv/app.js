dc26b22b1f12d9c478fec8ec144055a297b4c802
zzacal <zac.zacal@alaskaair.com>
2020-11-20 00:24:35 -0800

    Added pop and push methods

--- src/buffa-srv/app.js
+++ src/buffa-srv/app.js
@@ -3,14 +3,35 @@ const app = express()
 const port = 3000
 const HashTable = require('./hashTable.js')
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
 var records = new HashTable()
-records.add('test', ['hey','you'])
 
 app.get('/pop', (req, res) => {
   const key = req.query.key;
   const notes = records.search(key);
-  const line = notes.pop();
-  res.send(line);
+  if (notes) {
+    const line = notes.pop();
+    res.send(line);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.post('/push', (req, res) => {
+  const key = req.query.key;
+  let notes = records.search(key);
+  if (!notes) {
+    notes = []
+    records.add(key, notes);
+  }
+  const line = req.body.message;
+  notes.push(line);
+  res.sendStatus(200);
 })
 
 app.get('/', (req, res) => {
@@ -18,5 +39,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`buffa-srv started`)
 })
