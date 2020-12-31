107ba62e85a935992c9b8c781b79320629c6026b
zzacal <zac.zacal@alaskaair.com>
2020-12-31 00:52:04 -0800

    Image installs only prod dependencies

---
+++ src/buffa-ios/buffa/User/Register.swift
@@ -0,0 +1,20 @@
+//
+//  Register.swift
+//  buffa
+//
+//  Created by Zac Zacal on 12/30/20.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        Register()
+    }
+}
